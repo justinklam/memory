8938eef4f6ce66472f0974260af5073c128ee532
Justin Lam <justin.kh.lam@gmail.com>
2022-03-25 15:55:26 -0400

    correct css. formatting change

--- src/components/Settings/Settings.jsx
+++ src/components/Settings/Settings.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { CATEGORIES } from '../../constant';
+import { CATEGORIES } from './../../constant';
 import RadioBox from '../RadioBox/RadioBox.jsx';
 
 import styles from './Settings.module.css';
@@ -12,9 +12,12 @@ const Settings = () => {
           <h2>Settings</h2>
 
           <h4>Category:</h4>
-          <div className={`${styles.settings}`}>
+          <div className={`${styles.setting}`}>
             {CATEGORIES.map(item => {
-              <RadioBox key={item} name={item}/>
+              <RadioBox 
+                key={item} 
+                name={item}
+              />
             })}
           </div>
         </div>
