a29c46dcb6ecf7e643065e21ccf469f310da7866
Justin Lam <justin.kh.lam@gmail.com>
2022-03-25 15:44:40 -0400

    import Categories constant variable. iterate through the Categories constant and pass it into RadioBox component

--- src/components/Settings/Settings.jsx
+++ src/components/Settings/Settings.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import { CATEGORIES } from '../../constant';
+import RadioBox from '../RadioBox/RadioBox.jsx';
+
 import styles from './Settings.module.css';
 
 const Settings = () => {
     return (
-      <div className={`${styles.settings} frosted`}>
-        <h2>Settings</h2>
-      </div>
-    )
+        <div className={`${styles.settings} frosted`}>
+          <h2>Settings</h2>
+
+          <h4>Category:</h4>
+          <div className={`${styles.settings}`}>
+            {CATEGORIES.map(item => {
+              <RadioBox key={item} name={item}/>
+            })}
+          </div>
+        </div>
+    );
 };
 
 export default Settings;
