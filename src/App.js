4df9ab9392e7875e251abbfdab267dda241d3783
Justin Lam <justin.kh.lam@gmail.com>
2022-03-24 22:30:44 -0400

    add in search params for base_url. add in more modular search with buildURL function

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import Background from './components/Background';
 
-const BASE_URL = 'https://api.pexels.com/v1/search';
+const BASE_URL = 'https://api.pexels.com/v1/search?query=nature&orientation=square';
 
 function App() {
+  const buildURL = () => {
+    let url = new URL ('https://api.pexels.com/v1/search');
+    // adds the baseURL
+    url.search = new URLSearchParams({
+      query: 'nature',
+    });
+  };
+
   fetch(BASE_URL, {
     headers: {
       Authorization: process.env.REACT_APP_AUTH_KEY,
     }
   });
-    return (
-      <>
-        <Background />
-        <h1>Memory Game</h1>
-      </>
-    );
+
+  return (
+    <>
+      <Background />
+      <h1>Memory Game</h1>
+    </>
+  );
 };
 
 export default App;
