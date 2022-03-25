7bdee35332809392297defe8d98d6cf45a69a822
Justin Lam <justin.kh.lam@gmail.com>
2022-03-25 15:56:10 -0400

    add in logic to display the names prop

--- src/components/RadioBox/RadioBox.jsx
+++ src/components/RadioBox/RadioBox.jsx
@@ -3,8 +3,14 @@ import PropTypes from 'prop-types';
 
 import styles from './RadioBox.module.css';
 
-const RadioBox = () => {
-    return <p>RadioBox</p>;
+const RadioBox = ({name}) => {
+  console.log('name',name)
+    return (
+      <div className={`${styles.radioBox}`}>
+        <input type="radio" name={name} id={name} value={name}/>
+        <label htmlFor={name}>{name}</label>
+      </div>
+    )
 };
 
 export default RadioBox;
