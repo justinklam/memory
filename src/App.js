1f757607e67b0cd60630cea1f5619d61a9363e71
Justin Lam <justin.kh.lam@gmail.com>
2022-03-24 22:22:40 -0400

    add in base URL for API request and add in authorization key in a fetch request

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import Background from './components/Background';
 
+const BASE_URL = 'https://api.pexels.com/v1/search';
+
 function App() {
+  fetch(BASE_URL, {
+    headers: {
+      Authorization: process.env.REACT_APP_AUTH_KEY,
+    }
+  });
     return (
-        <>
-            <Background />
-            <h1>Memory Game</h1>
-        </>
+      <>
+        <Background />
+        <h1>Memory Game</h1>
+      </>
     );
-}
+};
 
 export default App;
