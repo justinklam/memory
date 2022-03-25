908893e89b4ef687be83f0acf6b4b1d0b8045dd5
Justin Lam <justin.kh.lam@gmail.com>
2022-03-24 22:39:01 -0400

    shift App.js logic into useGetImages.js

--- src/hooks/useGetImages.js
+++ src/hooks/useGetImages.js
@@ -1,5 +1,26 @@
 const BASE_URL = 'https://api.pexels.com/v1/search';
 
-const useGetImages = () => {};
+const getRandomPage = () => Math.round(Math.random() * (10-1) +1);
+
+const useGetImages = () => {
+  const buildURL = () => {
+    let url = new URL ('https://api.pexels.com/v1/search');
+    // adds the baseURL
+    url.search = new URLSearchParams({
+      query: 'nature',
+      orientation: 'square',
+      size: 'small',
+      per_page: 2,
+      page: getRandomPage()
+    });
+    return url;
+  };
+
+  fetch(buildURL(), {
+    headers: {
+      Authorization: process.env.REACT_APP_AUTH_KEY,
+    }
+  });
+};
 
 export default useGetImages;
