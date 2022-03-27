3f3061a6c55441ef9288aaa3e85e783791fa32a6
Justin Lam <justin.kh.lam@gmail.com>
2022-03-26 22:48:38 -0400

    pass in onChange as prop and have the input field trigger onChange

--- src/components/RadioBox/RadioBox.jsx
+++ src/components/RadioBox/RadioBox.jsx
@@ -3,12 +3,12 @@ import PropTypes from 'prop-types';
 
 import styles from './RadioBox.module.css';
 
-const RadioBox = ({name, selectedItem}) => {
+const RadioBox = ({name, selectedItem, onChange}) => {
   const isChecked = name === selectedItem;
   // console.log('name',name)
     return (
       <div className={`${styles.radioBox}`}>
-        <input type="radio" name={name} id={name} value={name} checked={isChecked}/>
+        <input type="radio" name={name} id={name} value={name} checked={isChecked} onChange={onChange}/>
         <label htmlFor={name}>{name}</label>
       </div>
     )
