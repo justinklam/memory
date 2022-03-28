b2622eed1be559c45e01f3a8239870e7c069a0da
Justin Lam <justin.kh.lam@gmail.com>
2022-03-28 17:32:56 -0400

    add in buttons to Counter.jsx

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -4,7 +4,14 @@ import PropTypes from 'prop-types';
 import './Counter.css';
 
 const Counter = () => {
-    return <p>Counter</p>;
+    return (
+      <div className="quantity">
+        <button className="minus">-</button>
+        <span className="quantity">8</span>
+        <button className="plus">+</button>
+      </div>
+      
+    )
 };
 
 export default Counter;
