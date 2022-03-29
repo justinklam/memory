21d4f594d0dbb25d7cf062d4de8830c90850e96d
Justin Lam <justin.kh.lam@gmail.com>
2022-03-29 00:48:03 -0400

    add in startGame prop to pass down to Settings component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
+// components
 import Background from './components/Background';
 import Settings from './components/Settings';
 
+// hook
 import useGetImages from './hooks/useGetImages';
 
 function App() {
   const images = useGetImages();
   // console.log({images});
 
+  const startGame = (options) => {
+    console.log({options});
+  };
+
   return (
     <>
       <Background />
         <h1>Memory Game</h1>
-      <Settings />
+      <Settings startGame={startGame}/>
     </>
   );
 };
