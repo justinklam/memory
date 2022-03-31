6e05118a377986ec45266fef82918964750189a3
Justin Lam <justin.kh.lam@gmail.com>
2022-03-31 17:31:14 -0400

    render and loop through Card component if it is not loading

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
 import Loader from './../Loader';
+import Card from './../Card';
 
 // hook
 import useGetImages from '../../hooks/useGetImages';
@@ -12,18 +13,21 @@ import styles from './Board.module.css';
 
 const Board = ({gameOptions}) => {
   const [isLoading, setIsLoading] = useState(true);
-
   const images = useGetImages(gameOptions);
-  console.log({images});
-
   const cards = useGameLogic(images);
-
+  
+  // console.log({images});
   // runs whenever we receive images
   useEffect(() => {
     if (images.length > 0) setIsLoading(false)
-  }, [images])
+  }, [images]);
   
-  return <div>{isLoading && <Loader />}</div>;
+  return (
+    <div>
+      {isLoading && <Loader />}
+      {!isLoading && cards.map(card => <Card key={card.uniqueId} card={card} />)}
+    </div>
+  );
 };
 
 export default Board;
