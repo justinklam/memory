7598588488c6f514ba53f8fc9729e4e2df39aa6c
Justin Lam <justin.kh.lam@gmail.com>
2022-04-01 15:29:05 -0400

    add in styling classes for cards

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,7 +4,14 @@ import PropTypes from 'prop-types';
 import styles from './Card.module.css';
 
 const Card = ({card}) => {
-    return <p>Card {card.uniqueId}</p>;
+    return (
+      <div className={`${styles.container}`}>
+      <div className={`${styles.card}`}>
+          <div className={`${styles.front}`}></div>
+          <div className={`${styles.back}`}></div>
+        </div>
+      </div>
+    )
 };
 
 export default Card;
