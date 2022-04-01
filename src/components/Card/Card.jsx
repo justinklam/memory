279e96c6a626c8fed212ef2b534c6c238dcbbcbb
Justin Lam <justin.kh.lam@gmail.com>
2022-04-01 15:32:12 -0400

    add in image component into card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+// components
+import Image from './../Image';
+
+// style
 import styles from './Card.module.css';
 
 const Card = ({card}) => {
@@ -9,6 +13,7 @@ const Card = ({card}) => {
       <div className={`${styles.card}`}>
           <div className={`${styles.front}`}></div>
           <div className={`${styles.back}`}></div>
+            <Image />
         </div>
       </div>
     )
